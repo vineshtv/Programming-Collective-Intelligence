e34375b34f1b7448481cac88ea0b913e2de1d5d6
vineshtv <tv.vinesh@gmail.com>
2017-11-25 23:15:14 +0530

    Added Pearson score calculation

--- CH-01/recommendations.py
+++ CH-01/recommendations.py
@@ -15,8 +15,8 @@ critics = {
         'Snakes on a Plane': 3.5,
         'Just My Luck': 1.5,
         'Superman Returns': 5.0,
-        'You, Me and Dupree': 3.0,
-        'The Night Listener': 3.5
+        'You, Me and Dupree': 3.5,
+        'The Night Listener': 3.0
     },
     'Michael Phillips': {
         'Lady in the Water': 3.0,
@@ -70,10 +70,50 @@ def sim_distance(prefs, person1, person2):
     sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2)
                             for item in prefs[person1] if item in prefs[person2]])
 
-    #return 1/(1 + sqrt(sum_of_squares))
-    return 1/(1 + sum_of_squares)
+    return 1/(1 + sqrt(sum_of_squares))
 
+
+#returns the Pearson correlation coefficient for p1 and p2
+def sim_pearson(prefs, person1, person2):
+    #Get the list of mutually rated items
+    si = {}
+
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item] = 1
+
+    #find the number if matching items
+    n = len(si)
+
+    #if they have no ratings in common return 0
+    if n == 0:
+        return 0
+
+    #Add up all the preferences
+    sum1 = sum([prefs[person1][it] for it in si])
+    sum2 = sum([prefs[person2][it] for it in si])
+
+    #Sum up the squares
+    sum1Sq = sum([pow(prefs[person1][it],2) for it in si])
+    sum2Sq = sum([pow(prefs[person2][it],2) for it in si])
+
+    #sum up the products
+    pSum = sum([prefs[person1][it] * prefs[person2][it] for it in si])
+
+    #Calulate the Pearson score
+    num = pSum - (sum1 * sum2 /n)
+    den = sqrt((sum1Sq - pow(sum1, 2)/n) * (sum2Sq - pow(sum2, 2)/n))
+
+    if den == 0:
+        return 0
+
+    r = num/den
+
+    return r
 
 if __name__ == "__main__":
     distance = sim_distance(critics, 'Lisa Rose', 'Gene Seymour')
     print "Euclidean distance score between Lisa Rose and Gene Seymour = ", distance
+
+    pearsonScore = sim_pearson(critics, 'Lisa Rose', 'Gene Seymour')
+    print "Pearson score for Lisa Rose and Gene Seymour = ", pearsonScore
